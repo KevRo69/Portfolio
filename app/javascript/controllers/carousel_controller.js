c4eec1f12337d18f5fe154325dd0d34121ee243c
kevin rossi <kevinrossi@free.fr>
2024-09-10 11:18:00 +0200

    continued carousel and fixed mistakes text

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="carousel"
 export default class extends Controller {
-  static targets = ["item"]
+  static targets = ["item", "description"]
 
   connect() {
     this.activeIndex = 1
@@ -23,8 +23,12 @@ export default class extends Controller {
     this.itemTargets.forEach((item, index) => {
       item.classList.remove("prev", "active", "next")
 
+      const bestunited = "<h3>Best United</h3><a href='http://www.bestunited.me', target='_blank'><i class='fa-solid fa-link'></i> Lien (App mobile)  </a><a href='https://www.youtube.com', target='_blank'><i class='fa-solid fa-video'></i> Vidéo pitch</a><p>Plateforme de vente de sneakers pour encourager le commerce local.</p><p>Gestion des stocks pour les commerçants avec scan de code-barre.</p><p>Projet final Le Wagon - Août 2024</p>"
+      const datefaker = "<h3>Date Faker</h3><a href='#'><i class='fa-solid fa-link'></i> Lien</a><p>Plateforme fictive type marketplace pour louer de faux dates pour des occasions spéciales.</p><p>Inscription, location et réservation avec formulaire.</p><p>Projet Le Wagon - Août 2024</p>"
+
       if (index === this.activeIndex) {
         item.classList.add("active")
+        this.descriptionTarget.innerHTML = eval(item.id)
       } else if (index === (this.activeIndex - 1 + this.itemTargets.length) % this.itemTargets.length) {
         item.classList.add("prev")
       } else if (index === (this.activeIndex + 1) % this.itemTargets.length) {
