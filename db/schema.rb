8ebbe077eef7ce7e07003ff13676fe9f42059827
kevin rossi <kevinrossi@free.fr>
2024-09-11 20:26:06 +0200

    modified navbar, bold, text, top-btn and contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_121032) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_11_181042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contacts", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "name"
     t.string "email"
-    t.string "phone"
-    t.string "title"
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
